dd344971a16416cf400749b7544aedd8af1eabb0
Lauri Kyttälä <lauri.kyttala@gmail.com>
2019-05-02 18:17:53 +0300

    trying to copy build to server side, changed baseUrl to Heroku

---
+++ osa_2/puhelinluettelo/build/precache-manifest.f03646e49978c49612ad120e19e5363d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9191bf2b6e33b7c68aae",
+    "url": "/static/js/main.60b9d8ac.chunk.js"
+  },
+  {
+    "revision": "717095a3ea21a6bbdee1",
+    "url": "/static/js/2.3d2452bb.chunk.js"
+  },
+  {
+    "revision": "9191bf2b6e33b7c68aae",
+    "url": "/static/css/main.d0a1c937.chunk.css"
+  },
+  {
+    "revision": "e57676acebae5d7c3a48ba9364315ca6",
+    "url": "/index.html"
+  }
+];
