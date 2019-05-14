79e2370d82252525de3e14d72e648f7814cecd09
Lauri Kyttälä <lauri.kyttala@gmail.com>
2019-05-14 11:57:14 +0300

    4.5 step3 describe blogs and favourite blog

--- osa_4_blogi/utils/list_helper.js
+++ osa_4_blogi/utils/list_helper.js
@@ -3,14 +3,20 @@ const dummy = blogs => {
 }
 
 const totalLikes = blogs => {
-  return 5
+  return blogs.map(blog => blog.likes).reduce((total, likes) => total + likes, 0)
 }
 
 const totalLikesOfMany = blogs => {
   return 36
 }
 
-module.exports = {
-  dummy, totalLikes, totalLikesOfMany
-}
+const mostLikes = blogs =>
+  blogs.reduce(
+    (favourite, current) =>
+      favourite === null || current.likes > favourite.likes ? current : favourite,
+    null
+  )
 
+module.exports = {
+  dummy, totalLikes, totalLikesOfMany, mostLikes
+}
