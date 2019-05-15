867bcc9bfa58eeb9f0b9da7146a9fd73cfdd61d9
Lauri Kyttälä <lauri.kyttala@gmail.com>
2019-05-15 15:26:35 +0300

    4.5 step3

--- osa_4/utils/list_helper.js
+++ osa_4/utils/list_helper.js
@@ -2,14 +2,15 @@ const dummy = blogs => {
   return 1
 }
 
-const totalLikes = blogs => {
-  return 5
+const totalLikesOfMany = blogs => {
+  return blogs.map(blog => blog.likes).reduce((total, likes) => total + likes, 0)
 }
 
-const totalLikesOfMany = blogs => {
-  return 36
-}
+const mostLikes = blogs =>
+  blogs.reduce((favourite, current) =>
+      favourite === null || current.likes > favourite.likes ? current : favourite,null
+  )
 
 module.exports = {
-  dummy, totalLikes, totalLikesOfMany
+  dummy, totalLikesOfMany, mostLikes
 }
