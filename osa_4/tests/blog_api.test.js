b699085e0c153a577a97cb6133c67a1d182d4982
Lauri Kyttälä <lauri.kyttala@gmail.com>
2019-05-15 19:51:06 +0300

    4.2 step1b

--- osa_4/tests/blog_api.test.js
+++ osa_4/tests/blog_api.test.js
@@ -1,15 +1,48 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
-
 const api = supertest(app)
+const Blog = require('../models/blog')
 
+
+const initialBlogs = [
+  {
+    title: 'React patterns',
+    author: 'Michael Chan',
+    url: 'https://reactpatterns.com/',
+    likes: 7,
+  },
+  {
+    title: 'Canonical string reduction',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+    likes: 12,
+  }
+]
+
+beforeEach(async () => {
+  await Blog.remove({})
+
+  let noteObject = new Blog(initialBlogs[0])
+  await noteObject.save()
+
+  noteObject = new Blog(initialBlogs[1])
+  await noteObject.save()
+})
 
 test('blogs are returned as json', async () => {
   await api
     .get('/api/blogs')
     .expect(200)
     .expect('Content-Type', /application\/json/)
+})
+
+test('all blogs are returned', async () => {
+  const response = await api
+    .get('/api/blogs')
+  console.log('res:', response.body[0].title)
+  expect(response.body.length).toBe(2)
+
 })
 
 afterAll(() => {
