678f7ce9ae266f2516418f4839e1a89403896c27
Lauri Kyttälä <lauri.kyttala@gmail.com>
2019-05-15 15:08:15 +0300

    4.3 & 4.4 step1 and step2

---
+++ osa_4/utils/list_helper.js
@@ -0,0 +1,15 @@
+const dummy = blogs => {
+  return 1
+}
+
+const totalLikes = blogs => {
+  return 5
+}
+
+const totalLikesOfMany = blogs => {
+  return 36
+}
+
+module.exports = {
+  dummy, totalLikes, totalLikesOfMany
+}
