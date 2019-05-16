c895e44adb645a0ed30858d60828d58cccd3e10a
Lauri Kyttälä <lauri.kyttala@gmail.com>
2019-05-16 11:50:49 +0300

    4.10 step3 - added test_helper

--- osa_4/tests/blog_api.test.js
+++ osa_4/tests/blog_api.test.js
@@ -2,33 +2,7 @@ const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
 const api = supertest(app)
-const Blog = require('../models/blog')
-
-
-const initialBlogs = [
-  {
-    title: 'React patterns',
-    author: 'Michael Chan',
-    url: 'https://reactpatterns.com/',
-    likes: 7,
-  },
-  {
-    title: 'Canonical string reduction',
-    author: 'Edsger W. Dijkstra',
-    url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
-    likes: 12,
-  }
-]
-
-beforeEach(async () => {
-  await Blog.remove({})
-
-  let noteObject = new Blog(initialBlogs[0])
-  await noteObject.save()
-
-  noteObject = new Blog(initialBlogs[1])
-  await noteObject.save()
-})
+const helper = require('./test_helper')
 
 test('blogs are returned as json', async () => {
   await api
@@ -47,6 +21,29 @@ test('all blogs are returned', async () => {
   const response = await api
     .get('/api/blogs')
   expect(response.body.length).toBe(2)
+})
+
+test('a valid blog can be added ', async () => {
+  const newBlog = {
+    title: 'Canonical string reduction',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+    likes: 12,
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+
+  const blogsAtEnd = await helper.blogsInDb()
+  const response = await api.get('/api/blogs')
+  const contents = blogsAtEnd.map(n => n.title)
+
+  expect(blogsAtEnd.length).toBe(helper.initialBlogs.length + 1)
+  expect(contents).toContain('Canonical string reduction')
 })
 
 afterAll(() => {
