db51b7e19dc79d1b0ef0c82679ecb1b470d7e2af
Lauri Kyttälä <lauri.kyttala@gmail.com>
2019-05-16 12:10:08 +0300

    added describe blogs to blogs_api.test

--- osa_4/tests/blog_api.test.js
+++ osa_4/tests/blog_api.test.js
@@ -4,46 +4,54 @@ const app = require('../app')
 const api = supertest(app)
 const helper = require('./test_helper')
 
-test('blogs are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
+describe('blogs are returned', () => {
+  test('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
 })
 
-test('test that id is defined', async () => {
-  const response = await api
-    .get('/api/blogs')
-  expect(response.body[0].id).toBeDefined()
+describe('test valid id exists', () => {
+  test('test that id is defined', async () => {
+    const response = await api
+      .get('/api/blogs')
+    expect(response.body[0].id).toBeDefined()
+  })
 })
 
-test('all blogs are returned', async () => {
-  const response = await api
-    .get('/api/blogs')
-  expect(response.body.length).toBe(2)
+describe('get all glogs', () => {
+  test('all blogs are returned', async () => {
+    const response = await api
+      .get('/api/blogs')
+    expect(response.body.length).toBe(2)
+  })
 })
 
-test('a valid blog can be added ', async () => {
-  const newBlog = {
-    title: 'Canonical string reduction',
-    author: 'Edsger W. Dijkstra',
-    url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
-    likes: 12,
-  }
+describe('add a valid blog', () => {
+  test('a valid blog can be added ', async () => {
+    const newBlog = {
+      title: 'Canonical string reduction',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+      likes: 12,
+    }
 
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
 
 
-  const blogsAtEnd = await helper.blogsInDb()
-  const response = await api.get('/api/blogs')
-  const contents = blogsAtEnd.map(n => n.title)
+    const blogsAtEnd = await helper.blogsInDb()
+    const response = await api.get('/api/blogs')
+    const contents = blogsAtEnd.map(n => n.title)
 
-  expect(blogsAtEnd.length).toBe(helper.initialBlogs.length + 1)
-  expect(contents).toContain('Canonical string reduction')
+    expect(blogsAtEnd.length).toBe(helper.initialBlogs.length + 1)
+    expect(contents).toContain('Canonical string reduction')
+  })
 })
 
 afterAll(() => {
