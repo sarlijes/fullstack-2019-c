dc07b5adbd472095ad19ca6ab26148fc5e4f8e03
Lauri Kyttälä <lauri.kyttala@gmail.com>
2019-05-21 15:50:42 +0300

    4.15 step4

--- osa_4/app.js
+++ osa_4/app.js
@@ -5,6 +5,7 @@ const cors = require('cors')
 const app = express()
 const mongoose = require('mongoose')
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 
 mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true })
 
@@ -12,5 +13,6 @@ app.use(cors())
 app.use(bodyParser.json())
 
 app.use('/api/blogs/', blogsRouter)
+app.use('/api/users/', usersRouter)
 
 module.exports = app
