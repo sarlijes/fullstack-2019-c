ab84f6c30902d4a88b70594a6331092f747cca79
Lauri Kyttälä <lauri.kyttala@gmail.com>
2019-06-10 16:26:40 +0300

    5.6 step6

--- osa_5_bloglist-frontend/src/components/Blog.js
+++ osa_5_bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,30 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from 'react'
+import index from '../index.css'
+
+const Blog = ({ blog }) => {
+  const [hidden, setVisible] = useState(false)
+
+  const toggleVisibility = () => {
+    setVisible(!hidden)
+  }
+
+  if (!hidden) {
+    return (<div className='blogStyle' onClick={toggleVisibility}>
+      {blog.title} {blog.author}
+    </div>)
+  }
+
+  return (
+    <div className='blogStyle'>
+      <div onClick={toggleVisibility}>
+        {blog.title}
+        <br></br> added by: {blog.author}
+        <br></br>
+        <a href={blog.url}>{blog.url}</a>
+        <br></br> {blog.likes}
+      </div>
+    </div>
+  )
+}
 
 export default Blog
