972df9fd588b907337fbe8c437b6cce64e1f9012
Lauri Kyttälä <lauri.kyttala@gmail.com>
2019-07-01 11:24:01 +0300

    5.20 step3 partly pending reset in BlogForm

--- osa_5_bloglist-frontend/src/services/blogs.js
+++ osa_5_bloglist-frontend/src/services/blogs.js
@@ -1,57 +1,10 @@
-// import axios from 'axios'
-// const baseUrl = '/api/blogs'
-
-// let token = null
-
-// const setToken = newToken => {
-//   token = `bearer ${newToken}`
-// }
-
-// const create = async newObject => {
-//   const config = {
-//     headers: { Authorization: token }
-//   }
-
-//   const response = await axios.post(baseUrl, newObject, config)
-//   return response.data
-// }
-
-// const getAll = () => {
-//   const request = axios.get(baseUrl)
-//   return request.then(response => response.data)
-// }
-
-// const getById = async (id) => {
-//   const response = await axios.get(`${baseUrl}/${id}`)
-//   return response.data
-// }
-
-// const update = (id, data) => {
-//   const config = { headers: { Authorization: token } }
-//   const request = axios.put(`${baseUrl}/${id}`, data, config)
-//   return request.then(response => response.data)
-// }
-
-// const remove = (id) => {
-//   const config = { headers: { Authorization: token } }
-//   const request = axios.delete(`${baseUrl}/${id}`, config)
-//   return request.then(response => response.data)
-// }
-
-// export default { getAll, getById, create, setToken, update, remove }
-
 import axios from 'axios'
 const baseUrl = '/api/blogs'
 
-let authHeader = null
+let token = null
 
-const setToken = token => {
-  authHeader = { authorization: `bearer ${token}` }
-}
-
-const getAll = () => {
-  const request = axios.get(baseUrl)
-  return request.then(response => response.data)
+const setToken = newToken => {
+  token = `bearer ${newToken}`
 }
 
 const create = async ({ newObject }) => {
@@ -59,30 +12,31 @@ const create = async ({ newObject }) => {
     method: 'post',
     url: baseUrl,
     data: newObject,
-    headers: authHeader
-  })
-
-  return response.data
-}
-
-const update = async ({ id, newDetails }) => {
-  const response = await axios({
-    method: 'put',
-    url: `${baseUrl}/${id}`,
-    data: newDetails,
-    headers: authHeader
-  })
-
-  return response.data
-}
-
-const remove = async ({ id }) => {
-  const response = await axios({
-    method: 'delete',
-    url: `${baseUrl}/${id}`,
-    headers: authHeader
+    headers: { Authorization: token }
   })
   return response.data
 }
 
-export default { getAll, create, setToken, update, remove }
+const getAll = () => {
+  const request = axios.get(baseUrl)
+  return request.then(response => response.data)
+}
+
+const getById = async (id) => {
+  const response = await axios.get(`${baseUrl}/${id}`)
+  return response.data
+}
+
+const update = (id, data) => {
+  const config = { headers: { Authorization: token } }
+  const request = axios.put(`${baseUrl}/${id}`, data, config)
+  return request.then(response => response.data)
+}
+
+const remove = (id) => {
+  const config = { headers: { Authorization: token } }
+  const request = axios.delete(`${baseUrl}/${id}`, config)
+  return request.then(response => response.data)
+}
+
+export default { getAll, getById, create, setToken, update, remove }
