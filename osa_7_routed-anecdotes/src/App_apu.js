8d7eb05ad9c1cb69974761d6ed8145f79855860f
Lauri Kyttälä <lauri.kyttala@gmail.com>
2019-07-26 18:32:05 +0300

    7.2 step2

--- osa_7_routed-anecdotes/src/App_apu.js
+++ osa_7_routed-anecdotes/src/App_apu.js
@@ -22,11 +22,7 @@ const AnecdoteList = ({ anecdotes }) => (
   <div>
     <h2>Anecdotes</h2>
     <ul>
-      {anecdotes.map(anecdote =>
-        <li key={anecdote.id}>
-          <Link to={`/anecdotes/${anecdote.id}`}>{anecdote.content}</Link>
-        </li>
-      )}
+      {anecdotes.map(anecdote => <li key={anecdote.id} >{anecdote.content}</li>)}
     </ul>
   </div>
 )
@@ -47,7 +43,6 @@ const About = () => (
 
 const Footer = () => (
   <div>
-    <br></br>
     Anecdote app for <a href='https://courses.helsinki.fi/fi/tkt21009'>Full Stack -sovelluskehitys</a>.
 
     See <a href='https://github.com/fullstack-hy2019/routed-anecdotes/blob/master/src/App.js'>https://github.com/fullstack-hy2019/routed-anecdotes/blob/master/src/App.js</a> for the source code.
@@ -58,7 +53,6 @@ const CreateNew = (props) => {
   const [content, setContent] = useState('')
   const [author, setAuthor] = useState('')
   const [info, setInfo] = useState('')
-  // const [votes, setVotes] = useState('')
 
 
   const handleSubmit = (e) => {
@@ -94,22 +88,13 @@ const CreateNew = (props) => {
 
 }
 
-const Anecdote = ({ anecdote }) => (
-  <div>
-    <p>{anecdote.content}</p>
-    <p>{anecdote.author}</p>
-    <p>has {anecdote.votes} votes</p>
-    <a href={`${anecdote.info}`}> {anecdote.info}</a>
-  </div >
-)
-
 const App = () => {
   const [anecdotes, setAnecdotes] = useState([
     {
       content: 'If it hurts, do it more often',
       author: 'Jez Humble',
       info: 'https://martinfowler.com/bliki/FrequencyReducesDifficulty.html',
-      votes: 1,
+      votes: 0,
       id: '1'
     },
     {
@@ -145,19 +130,18 @@ const App = () => {
   return (
     <div>
       <Router>
-        <div>
-          <h1>Software anecdotes</h1>
-          <Menu anecdotes={anecdotes} />
-          <Route exact path="/" render={() => <AnecdoteList anecdotes={anecdotes} />} />
-          <Route exact path="/anecdotes/:id" render={({ match }) =>
-            <Anecdote anecdote={anecdoteById(match.params.id)} />
-          } />
-          <Route path="/create" render={() => <CreateNew addNew={addNew} />} />
-          <Route path="/about" render={() => <About />} />
-          <Footer />
-        </div>
+        <h1>Software anecdotes</h1>
+        <Menu anecdotes={anecdotes} />
+        <Route exact path="/" render={() => <AnecdoteList anecdotes={anecdotes} />} />
+        <Route exact path="/anecdotes" render={() => <AnecdoteList anecdotes={anecdotes} />} />
+        <Route exact path="/anecdotes/:id" render={({ match }) =>
+          <AnecdoteList anecdote={anecdoteById(match.params.id)} />
+        } />
+        <Route path="/create" render={() => <CreateNew addNew={addNew} />} />
+        <Route path="/about" render={() => <About />} />
+        <Footer />
       </Router>
-    </div >
+    </div>
   )
 }
 
