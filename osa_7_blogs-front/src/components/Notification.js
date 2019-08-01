c6d47ffe403ce554ca1811f07b89725da2858e9a
Lauri Kyttälä <lauri.kyttala@gmail.com>
2019-08-01 16:57:41 +0300

    added store, notificationReducer under construction

--- osa_7_blogs-front/src/components/Notification.js
+++ osa_7_blogs-front/src/components/Notification.js
@@ -1,3 +1,35 @@
+// from osa_6 Redux-anecdotes
+
+// import React from 'react'
+// import { connect } from 'react-redux'
+
+// const Notification = (props) => {
+//   const style = {
+//     border: 'solid',
+//     padding: 10,
+//     borderWidth: 1,
+//     display:props.notification
+//   }
+//   return (
+//     <div style={style}>
+//       {props.notification}
+//     </div>
+//   )
+// }
+
+// const mapStateToProps = (state) => {
+//   return {
+//     notification: state.notification
+//   }
+// }
+
+// const ConnectNotification = connect(mapStateToProps)(Notification)
+
+// export default ConnectNotification
+
+
+// osa 2(vanha)
+
 import React from 'react'
 import index from '../index.css'
 
---
+++ osa_7_blogs-front/src/reducers/notificationReducer.js
@@ -0,0 +1,31 @@
+const reducer = (state = '', action) => {
+  switch (action.type) {
+    case 'SET_NOTICE':
+      return action.data
+    case 'SET_EMPTY':
+      return ''
+    default:
+      return state
+  }
+}
+
+export const newMessage = (message) => {
+  return { type: 'SET_NOTICE', data: { message } }
+}
+
+export const emptyMessage = (message) => {
+  return { type: 'SET_EMPTY' }
+}
+
+export const setMessage = (message, timeout) => {
+  return async dispatch => {
+    await dispatch({ type: 'SET_NOTICE', data: message })
+
+    timeout = timeout * 1000
+    setTimeout(() => {
+      dispatch({ type: 'SET_EMPTY' })
+    }, timeout)
+  }
+}
+
+export default reducer
