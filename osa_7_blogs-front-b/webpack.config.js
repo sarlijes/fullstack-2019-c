2a01ee41cbe8e62f847e7de6ecec07e7e4bf8efe
Lauri Kyttälä <lauri.kyttala@gmail.com>
2019-10-10 17:29:29 +0300

    7.15 eslint & 7.16 Webpack

---
+++ osa_7_blogs-front-b/webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path')
+const webpack = require('webpack')
+
+const config = (env, argv) => {
+  console.log('argv', argv.mode)
+
+  const backend_url = argv.mode === 'production'
+    ? 'http://localhost:3003'
+    : 'heroku'
+
+  return {
+    entry: ['@babel/polyfill', './src/index.js'],
+    output: {
+      path: path.resolve(__dirname, 'build'),
+      filename: 'main.js'
+    },
+    devServer: {
+      contentBase: path.resolve(__dirname, 'build'),
+      compress: true,
+      port: 3000,
+      proxy: { 'api/': backend_url }
+    },
+    devtool: 'source-map',
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          loader: 'babel-loader',
+          query: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        },
+        {
+          test: /\.css$/,
+          use: [
+            {
+              loader: 'style-loader'
+            },
+            {
+              loader: 'css-loader'
+            }
+          ]
+        }
+      ]
+    },
+    plugins: [
+      new webpack.DefinePlugin({
+        BACKEND_URL: JSON.stringify(backend_url)
+      })
+    ]
+  }
+}
+
+module.exports = config
